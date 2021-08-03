8a545ee535087ccf08cb9ed4f3f456e932ba24f3
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-08-03 18:34:07 +0900

    区切り線とリスナ設定コードの記述

--- app/src/main/java/com/websarva/wings/android/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/recyclerviewsample/MainActivity.kt
@@ -3,8 +3,15 @@ package com.websarva.wings.android.recyclerviewsample
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.*
 import com.google.android.material.appbar.CollapsingToolbarLayout
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +26,91 @@ class MainActivity : AppCompatActivity() {
         toolbarLayout.title= getString(R.string.toolbar_title)
         toolbarLayout.setExpandedTitleColor(Color.WHITE)
         toolbarLayout.setCollapsedTitleTextColor(Color.LTGRAY)
+
+        val lvMenu = findViewById<RecyclerView>(R.id.lvMenu)
+        val layout = LinearLayoutManager(this@MainActivity)
+        lvMenu.layoutManager=layout
+        val menuList = createTeishokuList()
+        val adapter = RecyclerListAdapter(menuList)
+        lvMenu.adapter =adapter
+        val decorator = DividerItemDecoration(this@MainActivity,layout.orientation)
+        lvMenu.addItemDecoration(decorator)
+
+    }
+
+    private inner class RecyclerListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var _tvMenuNameRow: TextView
+        var _tvMenuPriceRow: TextView
+
+        init{
+            _tvMenuNameRow =itemView.findViewById(R.id.tvMenuNameRow)
+            _tvMenuPriceRow =itemView.findViewById(R.id.tvMenuPriceRow)
+        }
+    }
+    private inner class RecyclerListAdapter(private val _listData: MutableList<MutableMap<String, Any>>): RecyclerView.Adapter<RecyclerListViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerListViewHolder{
+            val inflater = LayoutInflater.from(this@MainActivity)
+            val view =inflater.inflate(R.layout.row,parent,false)
+            view.setOnClickListener(ItemClickListener())
+            val holder = RecyclerListViewHolder(view)
+            return holder
+
+        }
+
+        override fun onBindViewHolder(holder: RecyclerListViewHolder, position: Int) {
+            val item = _listData[position]
+            val menuName = item["name"] as String
+            val menuPrice = item["price"] as Int
+            val menuPriceStr = menuPrice.toString()
+            holder._tvMenuNameRow.text = menuName
+            holder._tvMenuPriceRow.text = menuPriceStr
+        }
+
+        override fun getItemCount(): Int {
+            return _listData.size
+        }
+    }
+
+    private inner class ItemClickListener : View.OnClickListener{
+        override fun onClick(view: View){
+            val tvMenuName = view.findViewById<TextView>(R.id.tvMenuNameRow)
+            val menuName = tvMenuName.text.toString()
+            val msg =getString(R.string.msg_header) + menuName
+            Toast.makeText(this@MainActivity,msg,Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun createTeishokuList():MutableList<MutableMap<String,Any>>{
+        val menuList:MutableList<MutableMap<String,Any>> = mutableListOf()
+        var menu = mutableMapOf<String,Any>("name" to "から揚げ定食","price" to 800,"desc" to "若鶏のから揚げにサラダ、ご飯と味噌汁がつきます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ハンバーグ定食", "price" to 850, "desc" to "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        // 以下データ登録の繰り返し。
+        menu = mutableMapOf("name" to "生姜焼き定食", "price" to 850, "desc" to "すりおろし生姜を使った生姜焼きにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ステーキ定食", "price" to 1000, "desc" to "国産牛のステーキにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "野菜炒め定食", "price" to 750, "desc" to "季節の野菜炒めにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "とんかつ定食", "price" to 900, "desc" to "ロースとんかつにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "ミンチかつ定食", "price" to 850, "desc" to "手ごねミンチカツにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "チキンカツ定食", "price" to 900, "desc" to "ボリュームたっぷりチキンカツにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "コロッケ定食", "price" to 850, "desc" to "北海道ポテトコロッケにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "回鍋肉定食", "price" to 750, "desc" to "ピリ辛回鍋肉にサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "麻婆豆腐定食", "price" to 800, "desc" to "本格四川風麻婆豆腐にサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "青椒肉絲定食", "price" to 900, "desc" to "ピーマンの香り豊かな青椒肉絲にサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼き魚定食", "price" to 850, "desc" to "鰆の塩焼きにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menu = mutableMapOf("name" to "焼肉定食", "price" to 950, "desc" to "特性たれの焼肉にサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        return menuList
     }
 }
+
